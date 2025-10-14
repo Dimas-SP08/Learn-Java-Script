4faec5d233e8770a5cfac9abb37f8b9375bc5ee1
Dimas SP <satriapasha269@gmail.com>
2025-10-14 21:32:13 +0700

    learn JS

---
+++ pendahuluan/main.js
@@ -0,0 +1,40 @@
+// alert("hello")
+
+// prompt("Masukkan usia anda")
+
+// let usia=30
+// usia=66
+// console.log("bro lu adalah"+usia)
+// alert(usia)
+
+// let usia = prompt("berapa usia kamu")
+// alert('usia anda adalah' + usia)
+
+let nama = 'bambang'
+let usia = 89
+let tinggiBadan = 1.3
+let beratBadan 
+let pacar = null
+beratBadan = 8
+
+// supaya gak ada null
+// if (pacar == null) {
+//     pacar = 'bellum ppunya'
+// } else{
+//     pacar = 'punya'
+// }
+pacar = 1
+switch(pacar) {
+    case 1:
+        pacar = 'punya 1 aja'
+    break
+    case 2:
+        pacar = 'punya 2'
+    break
+    default:
+        pacar='belum punya pacar'
+    break
+
+}
+alert(`nama saya ${nama} usia saya itu ${40} berat ${beratBadan} pacar ${pacar}`)
+
